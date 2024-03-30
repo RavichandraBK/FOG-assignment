a627798ecfe0072fb8c5a5c490e59e3b6a35629b
RavichandraBK <ravichandrak2707@gmail.com>
2024-03-30 13:27:47 +0530

    Updated client

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import NavBar from './NavBar'
-import SideNav from './SideNav'
-import bfv from '../assets/BFV_Emblem.svg'
+import React from "react";
+import NavBar from "./NavBar";
+import SideNav from "./SideNav";
+import bfv from "../assets/BFV_Emblem.svg";
 const Home = () => {
   return (
     <>
-        <div>
-            <div className='flex'>
-                <div>
-                  <SideNav/>
-                  </div>
-                <div className=''><NavBar/></div>
-                <div className="absolute left-[800px] top-0 opacity-100">
+      <div>
+        <div className="flex">
+          <div>
+            <SideNav />
+          </div>
+          <div className="">
+            <NavBar />
+          </div>
+          <div className="absolute left-[800px] top-0 opacity-100">
             <img src={bfv} alt="" />
+          </div>
+          {/* <div className=""></div> */}
         </div>
-                <div className=''>Chant Hare krishna</div>
-            </div>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
